68e22139f81dd15ea11ae98874caf358b9f7340d
matlpg <126263519+matlpg@users.noreply.github.com>
2023-03-08 12:02:09 +0100

    Update main.py

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -6,3 +6,26 @@ file_name = "network.01.in"
 
 g = graph_from_file(data_path + file_name)
 print(g)
+
+def kruskal(graph) :
+    #tri des arêtes par ordre croissant de poids
+    edges=sorted(graph.edges,key=lambda x:x[2])
+    #Initialisation de la strucuture Union-Find
+    parent=list(range(graph.n))
+    
+    def find(x):
+        if parent[x]==x:
+            return x
+        parent[x]=find(parent[x])
+        return parent[x]
+    
+    def union(x,y):
+        parent[find(x)]=find(y)
+    
+    #construction de l'arbre couvrant de poids minimal
+    mst=Graph(graph.n)
+    for u,v,w in edges:
+        if find(u)!=find(v):
+            mst.add_edge(u,v,w)
+            union(u,v)
+    return mst
